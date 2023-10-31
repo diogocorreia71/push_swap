4f99c769e1265dad6f8c4edb1d02991f4f2994e8
Diogo Andre Dos Santos Correia <diogo_correia7@hotmail.com>
2023-10-31 09:30:42 +0000

    norminette fix

--- swap_command.c
+++ swap_command.c
@@ -6,30 +6,28 @@
 /*   By: diodos-s <diodos-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/05 10:00:53 by diodos-s          #+#    #+#             */
-/*   Updated: 2023/10/20 11:25:32 by diodos-s         ###   ########.fr       */
+/*   Updated: 2023/10/31 09:28:35 by diodos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void swap(t_node **head)
+void	swap(t_node **head)
 {
-    if (!head || !*head || !(*head)->next)
-        return;
+	t_node	*current;
+	t_node	*nextnode;
 
-    t_node *current = *head;
-    t_node *nextNode = current->next;
-
-    current->next = nextNode->next;
-    if (nextNode->next)
-        nextNode->next->prev = current;
-
-    nextNode->next = current;
-    current->prev = nextNode;
-
-    *head = nextNode;
+	current = *head;
+	nextnode = current->next;
+	if (!head || !*head || !(*head)->next)
+		return ;
+	current->next = nextnode->next;
+	if (nextnode->next)
+		nextnode->next->prev = current;
+	nextnode->next = current;
+	current->prev = nextnode;
+	*head = nextnode;
 }
-
 
 void	ss(t_node **a, t_node **b, bool checker)
 {
